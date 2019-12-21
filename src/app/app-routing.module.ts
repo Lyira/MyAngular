b64b95b34aea72110f999d69b27a5d07bf89ceb5
Lyira <49322964+Lyira@users.noreply.github.com>
2019-12-21 12:54:04 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FeedComponent} from './feed/feed.component';
+import {ArticleComponent} from './article/article.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'feed', component: FeedComponent},
+  {path: 'article', component: ArticleComponent},
+  {path: '', redirectTo: './feed', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
