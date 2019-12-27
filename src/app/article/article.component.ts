07b70c79f23871249026968ca2d6416593c7066e
Lyira <49322964+Lyira@users.noreply.github.com>
2019-12-27 15:33:48 +0200

    Update article.component.ts

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faStar } from '@fortawesome/free-regular-svg-icons';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
-
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import * as mockdata from '../mockdata/mockdata.component';
 
 @Component({
   selector: 'app-article',
@@ -13,9 +15,27 @@ export class ArticleComponent implements OnInit {
   faHeart = faHeart;
   faStar = faStar;
   faUserCircle = faUserCircle;
+  faChevronRight = faChevronRight;
+  faChevronLeft = faChevronLeft;
+
+  author;
+  authorComment;
+  image; 
+  likes; 
+  title;
+  text;
+  textComment;
+
   constructor() { }
 
   ngOnInit() {
+    this.title = mockdata.listArticle[3].title;
+    this.author = mockdata.listArticle[3].author;
+    this.authorComment = mockdata.listArticle[3].comments.author;
+    this.likes = mockdata.listArticle[3].reactionsCounts.likes;
+    this.image = mockdata.listArticle[3].image;
+    this.text = mockdata.listArticle[3].text;
+    this.textComment = mockdata.listArticle[3].comments.text;
   }
 
 }
