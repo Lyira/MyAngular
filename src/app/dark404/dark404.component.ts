b042e24f33e3448b18d4aba2eb55759e1f7bf77a
Lyira <49322964+Lyira@users.noreply.github.com>
2019-12-27 15:34:02 +0200

    Create dark404.component.ts

---
+++ src/app/dark404/dark404.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dark404',
+  templateUrl: './dark404.component.html',
+  styleUrls: ['./dark404.component.scss']
+})
+export class Dark404Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
