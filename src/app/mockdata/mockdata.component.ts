6b7577496ff79bc3daa372e288e5ab22eab26c43
Lyira <49322964+Lyira@users.noreply.github.com>
2019-12-27 15:35:21 +0200

    Create mockdata.component.ts

---
+++ src/app/mockdata/mockdata.component.ts
@@ -0,0 +1,144 @@
+
+export const listArticle = [
+  {
+    "id": '0001',
+    "author": 'Jane Paul',
+    "tags": [2],
+    "title": 'The Top 5 Programming languages Every Software Developer Should Learn',
+    "description": 'I have read somewhere that a programmer should learn a new programming language every year',
+    "text": "<p>I have read somewhere that a programmer should learn a new programming language every year ( I think it\'s code complete, not sure, though), but if you cannot do so, I suggest to at least learn following five programming language to do well in your career\.<\p><p>Every company loves a polyglot programmer and an all-rounder who is versatile enough to write a quick script and can also write complex Java programs. In fact, it\'s almost mandatory for a senior developer to learn more than one language.</p><p>Interviewers are giving more attention to an engineer who has good experience with multiple languages like C++ and Java goes quite well, so is Python and Java.</p><p>I personally like Software Engineers who have strong experience in either C++ or Java and can write scripts in Groovy, Perl, or Python.</p>",
+    "image": 'http://richinvest.biz/wp-content/uploads/2016/01/red-rounded-with-number-5-md.png',
+    "createdAt": '2019-06-02T04:55:19.411Z',
+    "updatedAt": '2019-12-09T04:55:19.411Z',
+    "reactionsCounts": {
+      "likes": 5,
+      "stars": 0,
+      "uni": 0,
+    },
+    "comments": {
+      "id": "000032565995",
+      "post" : "post",
+      "author": "User",
+      "text": "text323625+6+6",
+      "createdAt": '2019-06-02T04:55:19.411Z',
+      "updatedAt": '2019-12-09T04:55:19.411Z',
+    },
+    "reactionsAuthors": {
+      "likes": ['Jane Paul'],
+      "stars": [],
+      "uni": [],
+    },
+  },
+  {
+    "id": '0002',
+    "author": 'Kate Smit',
+    "tags": [2],
+    "title": '10 Tools Used by Java Programmers in Day to day life',
+    "description": 'Every trade has its tools, and Java programming is no exception.',
+    "text": "Every trade has its tools, and Java programming is no exception. In fact, good knowledge of tools and mastery of using them saves a lot of time in design, development, testing, debugging, and troubleshooting any production issue. To become a good developer, you must know the right tools for the right jobs. A developer without tools and tricks is like a toothless lion; hence you must devote some time to learning tools required for your day to day job. Many developers think that learning an IDE like Eclipse, NetBeans, or IntelliJ IDEA is enough, but that's just the start of the process. No doubt that IDEs are the most important tool ever created for programmers, but they are not the solution for everything.",
+    "image": 'https://s.dou.ua/img/announces/java8_zOfLHJC.png',
+    "createdAt": '2019-06-02T04:55:19.411Z',
+    "updatedAt": '2019-12-09T04:55:19.411Z',
+    "reactionsCounts": {
+      "likes": 10,
+      "stars": 0,
+      "uni": 0,
+    },
+    "comments": {
+      "id": "000032565995",
+      "post" : "post",
+      "author": "User",
+      "text": "text323625+6+6",
+      "createdAt": '2019-06-02T04:55:19.411Z',
+      "updatedAt": '2019-12-09T04:55:19.411Z',
+    },
+    "reactionsAuthors": {
+      "likes": ['Kate Smit'],
+      "stars": [],
+      "uni": [],
+    },
+  },
+  {
+    "id": '0003',
+    "author": 'Paul white',
+    "tags": [2],
+    "title": 'Top 3 Programming Languages Java Developers Can Learn',
+    "description": 'If you are a Java programmer and thinking to learn some news programming languages',
+    "text": "If you are a Java programmer and thinking to learn some news programming languages to expand your knowledge and skill, but not sure which programming language is better for you then you have come to the right place. In this article, I'll share 3 programming languages Java developers should learn in 2020 and why they should learn them. There is no surprise that all of these programming languages are JVM based programming languages but they all have different qualities which makes them unique and useful for a certain task. Also, being a Polyglot developer is good quality and often valued high on interviews. It also expands your thinking because different programming languages have different features which make certain things really easy.",
+    "image": 'https://nortsov.vn.ua/wp-content/uploads/2018/05/home-slide-1.jpg',
+    "createdAt": '2019-06-02T04:55:19.411Z',
+    "updatedAt": '2019-12-09T04:55:19.411Z',
+    "reactionsCounts": {
+      "likes": 20,
+      "stars": 0,
+      "uni": 0,
+    },
+    "comments": {
+      "id": "000032565995",
+      "post" : "post",
+      "author": "User",
+      "text": "text323625+6+6",
+      "createdAt": '2019-06-02T04:55:19.411Z',
+      "updatedAt": '2019-12-09T04:55:19.411Z',
+    },
+    "reactionsAuthors": {
+      "likes": ['Paul white'],
+      "stars": [],
+      "uni": [],
+    },
+  },
+  {
+    "id": '0004',
+    "author": 'Paul white',
+    "tags": [2],
+    "title": '2020 – THE YEAR OF ETHICS',
+    "description": 'If you are a Java programmer and thinking to learn some news programming languages',
+    "text": `2020 is the year of ethics. 
+    Clear vision is needed where we’re going.   
+    We now live in an age where politics, banking, our legal systems, and even borders are instituted primarily by the technologies we create. Their deployment, integrations, and agreements will rule our lives. There is no way around it. Where we’re going, borders are geographic, but limitless in the digital. 
+    We’re literally programming the future right now. And policies, agreements, and transactions are governed by something we can see or touch, but know is there, driving our freedoms or limiting them. 
+    While automation and increased mechanization impact jobs, the bigger impact will be on ownership, value distribution, surveillance, privacy, and data.
+    Blockchain technology sits at the center of technologies that are currently being integrated. It’s ledger for agreement, exchange, and incentivization. 
+    The code of ethics looks at 50 ethics in 10 areas to vote on and discuss where we’re going and what guardrails need to be created.
+    With this effort, an agreed upon document will be produced and a collaborative code of ethics will emerge as a living, working document to keep the conversation of what we’re creating for humanity. 
+    It’s important to remember that ALL PEOPLE ARE STAKEHOLDERS OF TECHNOLOGY. ALL. 
+    `,
+    "image": 'https://cdn.pixabay.com/photo/2019/11/25/17/05/new-years-eve-4652544_960_720.jpg',
+    "createdAt": '2019-06-02T04:55:19.411Z',
+    "updatedAt": '2019-12-09T04:55:19.411Z',
+    "reactionsCounts": {
+      "likes": 30,
+      "stars": 0,
+      "uni": 0,
+    },
+    "comments": {
+      "id": "000032565995",
+      "post" : "post",
+      "author": "kevin hank",
+      "text": "Generations Y and Z, soon to be the world’s most numerous consumers, are concerned about responsibly sourced and sustainable products. But when it comes to purchasing a gold chain or new hoop earrings, how often do they really know what they are buying?",
+      "createdAt": '2019-06-02T04:55:19.411Z',
+      "updatedAt": '2019-12-09T04:55:19.411Z',
+    },
+    "reactionsAuthors": {
+      "likes": ['Paul white'],
+      "stars": [],
+      "uni": [],
+    },
+  },
+  
+]
+
+export const user = [
+  {
+    "username": "Paul white" ,
+    "role": 'Student', 
+    "firstName": "Paul",
+    "lastName": "White",
+    "picture": "https://i.pinimg.com/originals/28/8a/0a/288a0a8bafb429a3a16a037869d85dc9.jpg",
+    "createdAt": '2019-12-09T04:55:19.411Z',
+    "updatedAt": '2019-12-09T04:55:19.411Z',
+    "lastLogin": 'qwer12rewq',
+    "posts": 'Post'
+
+  },
+]
