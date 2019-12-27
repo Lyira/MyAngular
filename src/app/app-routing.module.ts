7fecf0623aa88725b04d05f2fdce2488fc81f84b
Lyira <49322964+Lyira@users.noreply.github.com>
2019-12-27 15:33:23 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FeedComponent} from './feed/feed.component';
 import {ArticleComponent} from './article/article.component';
+import {Light404Component} from './light404/light404.component';
+import {Dark404Component} from './dark404/dark404.component';
+import {ProfileComponent} from './profile/profile.component';
+
 
 
 
 const routes: Routes = [
-  {path: 'feed', component: FeedComponent},
-  {path: 'article', component: ArticleComponent},
-  {path: '', redirectTo: './feed', pathMatch: 'full'},
+  { path: 'feed' , component: FeedComponent },
+  { path: 'profile' , component: ProfileComponent },
+  { path: 'article' , component: ArticleComponent },
+  { path: 'light404' , component: Light404Component },
+  { path: '404' , component: Dark404Component },
+  { path: 'dark404' , component: Dark404Component },
+  { path: '**' , component: Light404Component },
+  { path: '' , redirectTo: '/feed', pathMatch: 'full' }
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
+
