9bed84f8261bdaf74bc0a54fc72d899f82c17f5b
Lyira <49322964+Lyira@users.noreply.github.com>
2019-12-27 15:33:37 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { MockdataComponent } from './mockdata/mockdata.component';
 import { ArticleComponent } from './article/article.component';
 import { FeedComponent } from './feed/feed.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Light404Component } from './light404/light404.component';
+import { Dark404Component } from './dark404/dark404.component';
+import { ProfileComponent } from './profile/profile.component';
+ 
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
-    FeedComponent
+    FeedComponent,
+    Light404Component,
+    Dark404Component,
+    ProfileComponent,
+    // MockdataComponent
   ],
   imports: [
     BrowserModule,
