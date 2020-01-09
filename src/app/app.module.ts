1be8be1405d2d9efe35fa7f2e37bf91a3562af2e
Lyira <49322964+Lyira@users.noreply.github.com>
2020-01-09 15:58:24 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { MockdataComponent } from './mockdata/mockdata.component';
 import { ArticleComponent } from './article/article.component';
 import { FeedComponent } from './feed/feed.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Light404Component } from './light404/light404.component';
-import { Dark404Component } from './dark404/dark404.component';
+import { Light404Component } from './not-found/light404.component';
+import { Dark404Component } from './not-found/dark404.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
  
 
 @NgModule({
@@ -20,7 +20,7 @@ import { ProfileComponent } from './profile/profile.component';
     Light404Component,
     Dark404Component,
     ProfileComponent,
-    // MockdataComponent
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
