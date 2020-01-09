14a95c75598aaed1d0d5edf669dc3dd1d69a0a26
Lyira <49322964+Lyira@users.noreply.github.com>
2020-01-09 15:58:09 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FeedComponent} from './feed/feed.component';
 import {ArticleComponent} from './article/article.component';
-import {Light404Component} from './light404/light404.component';
-import {Dark404Component} from './dark404/dark404.component';
+import {Light404Component} from './not-found/light404.component';
+import {Dark404Component} from './not-found/dark404.component';
 import {ProfileComponent} from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'feed' , component: FeedComponent },
   { path: 'profile' , component: ProfileComponent },
   { path: 'article' , component: ArticleComponent },
+  { path: 'signup' , component: SignupComponent },
   { path: 'light404' , component: Light404Component },
   { path: '404' , component: Dark404Component },
   { path: 'dark404' , component: Dark404Component },
